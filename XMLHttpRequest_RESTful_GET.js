bc37ee7f8ca666013bcae93ef65e6d96278a916b
ttd2002 <118036040+ttd2002@users.noreply.github.com>
2023-09-26 21:47:29 +0700

    add

---
+++ XMLHttpRequest_RESTful_GET.js
@@ -0,0 +1,30 @@
+//Get all users
+/*
+    Đảm bảo cổng 8080 được kết nối khi chạy code
+*/
+var url = "http://localhost:8080/api/v1/users";
+
+//var url =
+'https://jsonplaceholder.typicode.com/todos';
+
+var xhr = new XMLHttpRequest()
+
+xhr.open('GET', url, true)
+
+xhr.onload = function() {
+
+    var users = JSON.parse(xhr.responseText);
+
+    if (xhr.readyState == 4 && xhr.status == "200") {
+
+        console.table(users);
+
+    } else {
+
+        console.error(users);
+
+    }
+
+}
+
+xhr.send(null);
